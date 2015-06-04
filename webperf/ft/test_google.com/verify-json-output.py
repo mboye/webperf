20ea7a717489473d27d3230d5a572f4b8a23bf7c
Magnus Boye <magnusboye@gmail.com>
2015-06-04 23:27:34 +0300

    FT: Check downloadTime metric
    - Sum of downloadTimes must not be zero.
    
    Change-Id: Iff99503b5dec6a4ea3eafd98c7568d92a40c0832

--- webperf/ft/test_google.com/verify-json-output.py
+++ webperf/ft/test_google.com/verify-json-output.py
@@ -3,9 +3,21 @@ import json
 import sys
 
 try:
-    json.load(open('webperf.output.json','r'))
+    data = json.load(open('webperf.output.json','r'))
+    download_time_sum = 0
+    for element in data['elements']:
+        http = element['http']
+        if not 'downloadTime' in http:
+            print "error: elements.http.downloadTime missing."
+            sys.exit(1)
+        download_time_sum += http['downloadTime']
+
+    if download_time_sum == 0:
+        print "error: sum of all downloadTimes is zero."
+        sys.exit(1)
+
     print "JSON integrity verified."
     sys.exit(0)
 except Exception as e:
-    print "JSON integrity check failed: {}".format(str(e))
+    print "error: JSON integrity check failed: {}".format(str(e))
     sys.exit(1)
